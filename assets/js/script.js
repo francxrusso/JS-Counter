2dd10922c7a69df2bc4af95a97af7934d55f9621
francxrusso <98696732+francxrusso@users.noreply.github.com>
2023-04-04 12:22:54 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,8 @@ const counter = document.createElement('h2');
 const plusButton = document.createElement('button');
 const resetButton = document.createElement('button');
 const minusButton = document.createElement('button');
+const minusTenButton = document.createElement('button');
+const plusTenButton = document.createElement('button');
 
 
 // Inizializzo il counter a 0
@@ -16,23 +18,36 @@ counter.innerText = count;
 plusButton.innerText = '+';
 resetButton.innerText = '↺';
 minusButton.innerText = '-';
+minusTenButton.innerText = '-10';
+plusTenButton.innerText = '+10';
 
 
 // Aggiungo classi agli elementi per CSS
 counter.className = 'counter';
 plusButton.className = 'plus';
+plusTenButton.className = 'plusTen';
 minusButton.className = 'minus';
+minusTenButton.className = 'minusTen';
 resetButton.className = 'reset';
+
 
 // Appendiamo gli elementi al div
 box.appendChild(counter);
 box.appendChild(minusButton);
 box.appendChild(resetButton);
 box.appendChild(plusButton);
+box.appendChild(plusTenButton);
+box.appendChild(minusTenButton);
 
 // Funzione aumenta
 function plus() {
     count++;
+    counter.innerText = count;
+}
+
+// Funzione aumenta di 10
+function plusTen() {
+    count + 10;
     counter.innerText = count;
 }
 
@@ -42,6 +57,16 @@ function minus() {
         return
     } else {
         count--;
+        counter.innerText = count;
+    }
+}
+
+// Funzione diminuisci di 10
+function minusTen() {
+    if (count === 0) { 
+        return
+    } else {
+        count - 10;
         counter.innerText = count;
     }
 }
